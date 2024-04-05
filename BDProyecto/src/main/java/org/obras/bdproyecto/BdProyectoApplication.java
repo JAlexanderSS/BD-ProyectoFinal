106480fe406adcbb5cd924f359eb95c60e4c4a11
JAlexanderSS <jalexanderss5003@gmail.com>
2024-04-05 17:04:15 -0600

    backend -v1

---
+++ BDProyecto/src/main/java/org/obras/bdproyecto/BdProyectoApplication.java
@@ -0,0 +1,13 @@
+package org.obras.bdproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdProyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdProyectoApplication.class, args);
+    }
+
+}
---
+++ BDProyecto/src/test/java/org/obras/bdproyecto/BdProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.obras.bdproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
